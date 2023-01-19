455ec050d0e14b9cece2ec59db41c34c4f6a320a
Cedric van Putten <me@bycedric.com>
2023-01-19 17:58:37 +0100

    feature(cli): also resolve the used metro version

--- packages/@expo/cli/src/start/server/metro/instantiateMetro.ts
+++ packages/@expo/cli/src/start/server/metro/instantiateMetro.ts
@@ -51,10 +51,10 @@ export async function instantiateMetroAsync(
     skipPlugins: true,
   });
 
-  logEventAsync('metro config', getMetroProperties(exp, metroConfig));
-
   const platformBundlers = getPlatformBundlers(exp);
   metroConfig = await withMetroMultiPlatformAsync(projectRoot, metroConfig, platformBundlers);
+
+  logEventAsync('metro config', getMetroProperties(projectRoot, exp, metroConfig));
 
   const {
     middleware,
